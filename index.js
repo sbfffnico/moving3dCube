b77203e17f446f9937d4556458fa2868cb721715
sbfffnico <nicoedrich91@gmail.com>
2022-10-12 02:48:01 -0500

    add buttons for rotations, added to do list file

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+let cube = document.getElementsByClassName("cube");
+let x = 0, y = 0, z = 0;
+
 function rotateCube(event) {
   if(event.buttons == 1) {
     event.preventDefault();
   }
-
-  var cube = document.getElementsByClassName("cube");
-  var x = event.clientX - window.innerWidth / 2;
-  var y = event.clientY - window.innerHeight / 2;
+  x = event.clientX - window.innerWidth / 2;
+  y = event.clientY - window.innerHeight / 2;
   var q = 0.42;
   var i;
 
@@ -17,9 +18,32 @@ function rotateCube(event) {
   }
 }
 
+// function rightSideUp() {
+//   x += 90;
+//   y += 90;
+//   //cube[0].style.transform = "rotateY(" + x + "deg) rotateX(" + y + "deg)";
+//   //cube[0].style.transform = "rotateY(" + x + "deg)";
+//   cube[0].style.transform = "rotateX(" + y + "deg)";
+// }
+
+function rotateX() {
+  x += 90;
+  cube[0].style.transform = "rotateX(" + x + "deg)";
+}
+function rotateY() {
+  y += 90;
+  cube[0].style.transform = "rotateY(" + y + "deg)";
+}
+function rotateZ() {
+  z += 90;
+  cube[0].style.transform = "rotateZ(" + z + "deg)";
+}
+
 $(".cube").mousedown(function () {
   $(".cube").mousemove(rotateCube);
 });
 $(document).mouseup(function () {
   $(".cube").unbind('mousemove');
 })
+
+
